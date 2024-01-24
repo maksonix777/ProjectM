d62874bf17b06da420e20a12301c1136cfd3d0b5
maksonix <maksonix@googlemail.com>
2024-01-24 02:08:42 +0100

    Commit message

--- src/pages/NotFoundPage/index.jsx
+++ src/pages/NotFoundPage/index.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Footer from '../../components/Footer'
 import notFoundPage from '../../ui/notfoundpage.jpg'
 import s from "./NotFoundPage.module.css" 
 import { Link } from 'react-router-dom'
