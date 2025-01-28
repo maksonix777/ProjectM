cbbdfe565fa7d89fa3f04346a90d49f9f44fbe5a
maksonix777 <128647093+maksonix777@users.noreply.github.com>
2025-01-28 20:53:41 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ import { persistor, store } from './store';
 import { PersistGate } from 'redux-persist/integration/react';
 
 
-// export const ROOT_URL = 'https://telran-backend.onrender.com'
-export const ROOT_URL = 'http://localhost:3333'
+ export const ROOT_URL = 'https://telran-backend.onrender.com'
+//export const ROOT_URL = 'http://localhost:3333'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
